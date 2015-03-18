3899a8ab20887c713ee4f7d20d252b62c047fac1
Stefano Martina <stefano.martina@gmail.com>
2015-03-18 12:21:37 +0100

    Almost finished slides coffee seminar, modified cleaning script

--- cleanTex.sh
+++ cleanTex.sh
@@ -2,11 +2,13 @@
 
 if [ $# -eq 0 ]; then
     rm *.aux *.log *.nav *.out *.snm *~ *.toc *.vrb 2> /dev/null
+    rm -fr auto
 elif [[ $# == 1 && $1 == "-a" ]]; then
     dirs=($(ls -d */))
     for dir in "${dirs[@]}"; do
         cd $dir
         rm *.aux *.log *.nav *.out *.snm *~ *.toc *.vrb 2> /dev/null
+        rm -fr auto
         cd ..
     done
 else
